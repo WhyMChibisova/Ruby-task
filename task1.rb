656293a3f661597f03261524e284fe4feb3b9b4b
WhyMChibisova <chibisova3586@gmail.com>
2022-12-12 23:14:39 +0300

    Add task1 with one method of encrypt

--- task1.rb
+++ task1.rb
@@ -1,21 +1,25 @@
 class Cipher
-
   DICTIONARY = "abcdefghijklmnopqrstuvwxyz"
   DICTIONARY_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   attr_accessor :key
 
-  def encrypt(file_path)
-    file = File.open(file_path, "r")
-    text = file.read()
-    file.close()
+  def crypt(file_path)
+    begin
+      file = File.open(file_path, "r")
+      text = file.read()
+    rescue
+      puts "File error"
+    ensure
+      file.close()
+    end
     result = ""
-
-    if @key == nil
+    if @key.nil?
       puts "Enter the encryption key: "
       @key = gets.to_i
+    else @key = -@key
     end
 
-    text.split('').each do |letter|
+    text.each_char do |letter|
       if DICTIONARY.include?(letter)
         index = DICTIONARY.index(letter)
         result += DICTIONARY[(index + @key) % DICTIONARY.length()]
@@ -26,50 +30,28 @@ class Cipher
       end
     end
     puts result
+
+    begin
     file = File.open(file_path, "w")
     file.print(result)
-    file.close()
-  end
-
-  def decrypt(file_path)
-    file = File.open(file_path, "r")
-    text = file.read()
-    file.close()
-    result = ""
-
-    if @key == nil
-      puts "Enter the encryption key: "
-      @key = gets.to_i
+    rescue
+      puts "File error"
+    ensure
+      file.close()
     end
-
-    text.split('').each do |letter|
-      if DICTIONARY.include?(letter)
-        index = DICTIONARY.index(letter)
-        result += DICTIONARY[(index - @key) % DICTIONARY.length()]
-      elsif DICTIONARY_UPPER.include?(letter)
-        index = DICTIONARY_UPPER.index(letter)
-        result += DICTIONARY_UPPER[(index - @key) % DICTIONARY_UPPER.length()]
-      else
-        result += letter
-      end
-    end
-    puts result
-    file = File.open(file_path, "w")
-    file.print(result)
-    file.close()
   end
 
 end
 
 cipher = Cipher.new()
-puts cipher.encrypt("text.txt")
+puts cipher.crypt("text.txt")
 puts "Encryption passed"
-puts cipher.decrypt("text.txt")
+puts cipher.crypt("text.txt")
 puts "Decryption passed"
 
 cipher1 = Cipher.new()
 cipher1.key = 5
-puts cipher1.encrypt("text.txt")
+puts cipher1.crypt("text.txt")
 puts "Encryption passed"
-puts cipher1.decrypt("text.txt")
+puts cipher1.crypt("text.txt")
 puts "Decryption passed"
